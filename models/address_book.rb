31e04f688f77ffc6fe6f749207a32252db3e2132
Andrea Mansur <apmansur1993@gmail.com>
2017-05-21 16:19:48 +0000

    add search method

--- models/address_book.rb
+++ models/address_book.rb
@@ -38,6 +38,39 @@ class AddressBook
     end
   end
   
+  
+  def binary_search(name)
+      
+     lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     return nil 
+  end
+  
+  def iterative_search(name)
+      iteration = 0
+    while iteration < entries.length
+        if @entries[iteration].name == name
+            return entries[iteration]
+        else
+            iteration += 1
+        end
+    end
+    return nil
+            
+  end
 
  
  
